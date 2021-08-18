259f8026fe94c21143edcc0933079e6d0fd12cb1
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-08-19 03:36:08 +0400

    final update

--- src/main.ts
+++ src/main.ts
@@ -13,20 +13,20 @@ platformBrowserDynamic()
   .catch((err) => console.error(err));
 
 /* Disable inspect element opener short cuts */
-document.onkeydown = (e) => {
-  if (e.keyCode == 123) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    return false;
-  } else return;
-};
+// document.onkeydown = (e) => {
+//   if (e.keyCode == 123) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//     return false;
+//   } else return;
+// };
